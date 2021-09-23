0623e344febcf6ec7efe9e5c2f9039c31f8cf4ee
JAHH20 <72756336+JAHH20@users.noreply.github.com>
2021-09-22 23:02:22 -0500

    Comentario de prueba

--- app/src/main/java/com/example/encargalo/PRUEBA.java
+++ app/src/main/java/com/example/encargalo/PRUEBA.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PRUEBA extends AppCompatActivity {
-
+//Comentario de prueba
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
